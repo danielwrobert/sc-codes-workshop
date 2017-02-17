af9387680a46a89d2700f7b29df6b92bb04c1635
Daniel W. Robert <danielwrobert@gmail.com>
2017-02-17 10:59:04 -0500

    Fixed word breaking. Removed un-used scripts from enqueue.

--- sc-codes-theme/functions.php
+++ sc-codes-theme/functions.php
@@ -94,8 +94,6 @@ add_action( 'widgets_init', 'sc_codes_widgets_init' );
 function sc_codes_scripts() {
 	wp_enqueue_style( 'sc-codes-bootstrap-styles', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
 	wp_enqueue_style( 'sc_codes-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'sc_codes-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'sc_codes-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
