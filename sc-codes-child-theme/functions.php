ecb7f5ab02d9517683a1415bbc81788a4aff8103
Daniel W. Robert <danielwrobert@gmail.com>
2017-08-31 12:31:38 -0400

    Removed child stylesheet enqueue in theme.
    This is actually done by default and the previous code actually loaded
    the child stylesheet twice.

--- sc-codes-child-theme/functions.php
+++ sc-codes-child-theme/functions.php
@@ -10,6 +10,5 @@
  */
 function sc_codes_child_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 }
 add_action( 'wp_enqueue_scripts', 'sc_codes_child_styles' );
